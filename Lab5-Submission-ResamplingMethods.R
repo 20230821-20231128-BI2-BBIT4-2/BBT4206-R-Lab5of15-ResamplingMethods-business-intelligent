6cfd0ec942206284a2d4d1b3c1ff6dc3833132bc
Joseph Watunu <justinwatunu@gmail.com>
2023-10-16 15:10:52 +0300

    bootstrapping train control

--- Lab5-Submission-ResamplingMethods.R
+++ Lab5-Submission-ResamplingMethods.R
@@ -68,3 +68,55 @@ train_index <- createDataPartition(PimaIndiansDiabetes$diabetes,
                                    p=0.80, list=FALSE)
 diabetes_dataset_train <- PimaIndiansDiabetes[train_index,]
 diabetes_dataset_test <- PimaIndiansDiabetes[-train_index,]
+
+#performing naive bayes
+
+diabetes_dataset_nb_e1071 <-
+  e1071::naiveBayes(diabetes~ pregnant+ glucose+ pressure+
+                      triceps+ insulin+ mass+ pedigree+ age,
+                    data=diabetes_dataset_train)
+
+#testing the model using the test dataset
+
+prediction_nb_e1071 <-
+  predict(diabetes_dataset_nb_e1071,
+          diabetes_dataset_test[, c("pregnant", "glucose",
+                                    "pressure","triceps","insulin", "mass",
+                                    "pedigree","age")
+            
+          ])
+
+print(prediction_nb_e1071)
+
+caret::confusionMatrix(prediction_nb_e1071,
+                       diabetes_dataset_test[, c("pregnant", "glucose",
+                                                 "pressure","triceps","insulin", "mass",
+                                                 "pedigree","age","diabetes")]$diabetes)
+#visualizing the prediction
+plot(table(prediction_nb_e1071,
+           diabetes_dataset_test[, c("pregnant", "glucose",
+                                     "pressure","triceps","insulin", "mass",
+                                     "pedigree","age","diabetes")]$diabetes))
+
+
+#bootstrapping train control
+train_control <- trainControl(method = "boot", number = 500)
+
+str(diabetes_dataset_test)
+
+
+diabetes_dataset_model_lm <- # nolint
+  caret::train(diabetes ~
+                 pregnant+ glucose+ pressure+
+                 triceps+ insulin+ mass+ pedigree+ age,
+               data = diabetes_dataset_train,
+               trControl = train_control,
+               na.action = na.omit, method = "glm", metric = "Accuracy")
+
+
+predictions_lm <- predict(diabetes_dataset_model_lm,
+                          diabetes_dataset_test[, 1:8])
+
+## 4. View the RMSE and the predicted values for the 12 observations ----
+print(diabetes_dataset_model_lm)
+print(predictions_lm)
